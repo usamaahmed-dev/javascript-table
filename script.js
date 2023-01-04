c13a56c524431316f2f5af13d56718673bea4359
Usama Ahmed <usamaahmed332675@gmail.com>
2023-01-04 00:00:42 -0800

    prompt mesage changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let username = prompt("Please enter your name", "");
 alert("Hello " + username);
 
 function getAnumber() {
-    let num = prompt("Enter a number");
+    let num = prompt("Enter a number JALDI");
     for (let i = 1; i <= 10; i++) {
         console.log(num + "x" + i + "=" + i * num);
     }
